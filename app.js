9837f83ef0da81ff5b875e5bb6438f264617d1d3
jhansi20 <63865253+jhansi20@users.noreply.github.com>
2023-11-14 19:39:19 +0530

    Update app.js
    Added customer not found functionality as well

--- app.js
+++ app.js
@@ -7,30 +7,60 @@ const server = http.createServer((req, res) => {
   
   if (req.method === 'GET') {
     if (req.url.includes('/get')) {
+      const customerID = parseInt(req.url.split('/')[2]);
+      if(customerID){
+        const custExists=customers.find((customer)=>customer.id===customerID);
+        if(!custExists){
+          res.writeHead(200,{'Content-Type': 'application/plain-text'});
+          res.end('Customer does not exist');
+        }
+        else{
+        res.writeHead(200,{'Content-Type': 'application/json'});
+        res.end(JSON.stringify(custExists));
+        }
+      }
+      else{
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(customers));
+      }
     }
   }
   if (req.method === 'POST') {
     if (req.url.includes('/post')){
       let body;
+      let custId;
       req.on('data', (chunk) => {
         body = JSON.parse(chunk.toString());
-      });
+        custId = body.id;
+        const custExists=customers.find((customer)=>customer.id===custId);
+      if(!custExists){
       req.on('end', () => {
         customers.push(body);
         res.writeHead(200, { 'Content-Type': 'application/plain-text' });
         res.end('Customer added successfully');
     });
+  }
+  else{
+    res.writeHead(404, { 'Content-Type': 'application/plain-text' });
+        res.end('Customer already exists!');
+  }
+      });
 }
 }
 if (req.method === 'DELETE') {
     if (req.url.includes('/delete')) {
       const customerID = parseInt(req.url.split('/')[2]);
+      const custToDel=customers.find((customer)=>customer.id===customerID);
+      if(!custToDel){
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Customer not found');
+      }
+      else{
       const updatedProducts = customers.filter((customer) => customer.id !== customerID);
       customers = updatedProducts;
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Customer deleted successfully');
+      }
   }
 }
 if(req.method==='PUT'){
@@ -50,10 +80,11 @@ if(req.method==='PUT'){
                 res.end(JSON.stringify(customers));
             });
         }
-      } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Customer not found');
-      }
+        else {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('Customer not found');
+        }
+      } 
     }
 });
 const PORT = process.env.PORT || 3000;
